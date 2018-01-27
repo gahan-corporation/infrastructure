200eb3b17b0bd30d18da6dca8e392835de8265af
Xander Harris <duchess@gahan-corporation.com>
2018-01-27 15:27:02 -0800

    Syntax matters. See #2.

--- aws/nginx.tf
+++ aws/nginx.tf
@@ -1,8 +1,10 @@
 provider "aws" {
+  access_key = ""
+  secret_key = ""
   region = "us-west-2"
+}
 
-  resource "aws_instance" "nginx" {
-    ami           = "ami-c655e2be"
-    instance_type = "t2.micro"    
-  }
+resource "aws_instance" "nginx" {
+  ami           = "ami-c655e2be"
+  instance_type = "t2.micro"    
 }
