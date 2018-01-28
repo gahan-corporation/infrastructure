075b94476e1bc805cb9ab5dd918b6ef66eb03c7d
Xander Harris <duchess@gahan-corporation.com>
2018-01-27 18:28:29 -0800

    Update amis. See #1.

--- aws/oregon.tf
+++ aws/oregon.tf
@@ -5,6 +5,13 @@ provider "aws" {
 }
 
 resource "aws_instance" "oregon" {
-  ami           = " ami-9abea4fb"
   instance_type = "t2.large"
+}
+
+resource "aws_instance" "nginx" {
+  ami = "ami-9557e0ed"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "nginx" {
 }
