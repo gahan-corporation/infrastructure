ffe75b864930ab17711c61a3521e28b5d921cbfa
Xander Harris <duchess@gahan-corporation.com>
2018-01-27 20:25:09 -0800

    Add key pair resource. See #1.

--- aws/oregon.tf
+++ aws/oregon.tf
@@ -13,18 +13,23 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "general-disarray" {
-  key_name   = "general_disarray"
+  key_name = "general_disarray"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCraOI8EdofAFS57yfO13TRFSWb2Xpcr4SpjuhSRlSOz0YX3wax3kxGaZgFPqYX7nhptOj6+zbsPMXrZioK/ScgoEVInWMyr5dQA70AKmYKD7I9CYejLop5cTxwCuJHfbiBD4xyw+rdKs/SH55zj+k7oKvFFlndP6sImMvfaUQIDimxcMEud7SMDKwYA6MwRe6gA+uSzLKaUZmepWx/OnunJRl36vu07fXT5snizKv2iVSSt8qxv2QEE1iMCied/QlGSOEjF9qxeiqlE0KGZ573Ms4AIHyjJbfzKnsgM67rfss91KYffZ18V17xM/SzyUjcO/w45VYAEWNVQggtXiFZ general_disarray"
 }
 
 resource "aws_instance" "oregon" {
   ami = "ami-9abea4fb"
   instance_type = "t2.large"
+
+  tags {
+    name = "oregon"
+  }
 }
 
 resource "aws_instance" "docker" {
   ami = "ami-9557e0ed"
   instance_type = "t2.micro"
+  key_name = "professor-chaos"
 
   provisioner "local-exec" {
     command = "ap nginx/provision.yml" 
