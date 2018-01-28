1634df1af64259a4a847759313e58dbb8417317e
Xander Harris <duchess@gahan-corporation.com>
2018-01-27 22:32:53 -0800

    Add default security group. See #1.

--- aws/security.tf
+++ aws/security.tf
@@ -13,3 +13,29 @@ resource "aws_security_group" "mail" {
     Name = "mail"
   }
 }
+
+resource "aws_security_group" "default" {
+  name = "default"
+  description = "Default security group."
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443 
+    to_port = 443 
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 0
+    to_port = 64999 
+    protocol = "tcp"
+    cidr_blocks = ["172.249.87.205/32"]
+  }
+  tags {
+    Name = "default"
+  }
+}
