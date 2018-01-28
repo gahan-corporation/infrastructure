8dcb1dea54bf12950f06712e45b6ebfed1b5d81e
Xander Harris <duchess@gahan-corporation.com>
2018-01-28 09:18:16 -0800

    Update state. See #5.

--- docker/container.tf
+++ docker/container.tf
@@ -2,9 +2,19 @@ provider "docker" {
   host = "tcp://127.0.0.1:2376/"
 }
 
+resource "docker_volume" "etc-nginx" {
+  host_path = "nginx/files/etc"
+  container_path = "/etc/nginx"
+}
+
+resource "docker_network" "gcorpnet" {
+  name = "gcorp"
+}
+
 resource "docker_container" "nginx" {
   image = "${docker_image.di-nginx.name}"
   name  = "nginx"
+  networks = ["gcorp"]
 }
 
 resource "docker_image" "di-nginx" {
