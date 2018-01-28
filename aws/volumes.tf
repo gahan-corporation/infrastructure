d8dd8b0ebe868bf570c265b4157a08fe07100667
Xander Harris <duchess@gahan-corporation.com>
2018-01-28 04:33:05 -0800

    Update volume. See #1.

--- aws/volumes.tf
+++ aws/volumes.tf
@@ -6,19 +6,11 @@ resource "aws_ebs_volume" "oregon-sda" {
   }
 }
 
-resource "aws_ebs_volume" "oregon-gprof" {
-  availability_zone = "us-west-2c"
-  size = 300
-  tags {
-    Name = "gprof"
-  }
-}
-
-resource "aws_ebs_volume" "oregon-pgwal" {
+resource "aws_ebs_volume" "pg_wal" {
   availability_zone = "us-west-2c"
   size = 100
   tags {
-    Name = "pgwal"
+    Name = "pg_wal"
   }
 }
 
