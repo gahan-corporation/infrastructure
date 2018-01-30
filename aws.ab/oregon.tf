c6dc0d62847fe436c57a681d2ab4795b24ac0318
Xander Harris <duchess@gahan-corporation.com>
2018-01-30 02:27:01 -0800

    New aws. See #19.

---
+++ aws.ab/oregon.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "archive.my-ubox.com"
+    key    = "aws.tfstate"
+    region = "us-west-2"
+  }
+}
+
+variable "abio_aws_access_key_id" {}
+variable "abio_aws_secret_access_key" {}
+
+provider "aws" {
+  access_key = "${var.abio_aws_access_key_id}"
+  secret_key = "${var.abio_aws_secret_access_key}"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "abio" {
+  provider = "aws.west"
+  ami_id = "ami-6e1a0117" 
+  instance_id = "i-0b0b8c73e92aabc59"
+}
