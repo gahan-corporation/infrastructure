94acedd3f60ba21c889b30d84e39c5ca510dc716
Xander Harris <duchess@gahan-corporation.com>
2018-01-30 00:54:29 -0800

    Add jess env vars. See #17.

--- heroku/rsum.tf
+++ heroku/rsum.tf
@@ -9,6 +9,8 @@ terraform {
 variable "heroku_api_key" {}
 variable "dev_aws_secret_access_key" {}
 variable "dev_aws_access_key_id" {}
+variable "jess_aws_secret_access_key" {}
+variable "jess_aws_access_key_id" {}
 
 provider "heroku" {
   email   = "xander@gahancorporation.com"
@@ -25,6 +27,23 @@ resource "heroku_app" "dev" {
     DISABLE_COLLECTSTATIC = 1 
     DJANGO_SETTINGS_MODULE = "rsum.settings"
     RSUM_ENV = "dev"
+  }
+
+  buildpacks = [
+    "heroku/python"
+  ]
+}
+
+resource "heroku_app" "jess" {
+  name   = "rsum-jess"
+  region = "us"
+
+  config_vars {
+    AWS_ACCESS_KEY_ID = "${var.jess_aws_access_key_id}"
+    AWS_SECRET_KEY_ID = "${var.jess_aws_secret_access_key}"
+    DISABLE_COLLECTSTATIC = 1 
+    DJANGO_SETTINGS_MODULE = "rsum.settings"
+    RSUM_ENV = "jess"
   }
 
   buildpacks = [
