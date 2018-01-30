2a36c52f7f3d7963a7427f391b8b50e7a7b35110
Xander Harris <duchess@gahan-corporation.com>
2018-01-30 00:28:23 -0800

    Update env. See #17.

--- heroku/rsum.tf
+++ heroku/rsum.tf
@@ -1,9 +1,16 @@
+variable "heroku_api_key" {}
+
+provider "heroku" {
+  email   = "xander@gahancorporation.com"
+  api_key = "${var.heroku_api_key}"
+}
+
 resource "heroku_app" "dev" {
   name   = "rsum-dev"
   region = "us"
 
   config_vars {
-    FOOBAR = "baz"
+    DISABLE_COLLECTSTATIC = 1 
   }
 
   buildpacks = [
