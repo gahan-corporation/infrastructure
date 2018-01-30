ffcb796f5c9ee4d466e7f05724d3985bf1eb5fed
Xander Harris <duchess@gahan-corporation.com>
2018-01-30 01:04:30 -0800

    Add xander See #17>

--- heroku/rsum.tf
+++ heroku/rsum.tf
@@ -11,6 +11,8 @@ variable "dev_aws_secret_access_key" {}
 variable "dev_aws_access_key_id" {}
 variable "jess_aws_secret_access_key" {}
 variable "jess_aws_access_key_id" {}
+variable "xander_aws_secret_access_key" {}
+variable "xander_aws_access_key_id" {}
 
 provider "heroku" {
   email   = "xander@gahancorporation.com"
@@ -44,6 +46,23 @@ resource "heroku_app" "jess" {
     DISABLE_COLLECTSTATIC = 1 
     DJANGO_SETTINGS_MODULE = "rsum.settings"
     RSUM_ENV = "jess"
+  }
+
+  buildpacks = [
+    "heroku/python"
+  ]
+}
+
+resource "heroku_app" "xander" {
+  name   = "rsum-xander"
+  region = "us"
+
+  config_vars {
+    AWS_ACCESS_KEY_ID = "${var.xander_aws_access_key_id}"
+    AWS_SECRET_KEY_ID = "${var.xadner_aws_secret_access_key}"
+    DISABLE_COLLECTSTATIC = 1 
+    DJANGO_SETTINGS_MODULE = "rsum.settings"
+    RSUM_ENV = "xander"
   }
 
   buildpacks = [
