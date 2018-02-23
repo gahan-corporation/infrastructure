98d82c90641dfe0c6368a8f45a166bc0702eed41
Xander Harris <duchess@gahan-corporation.com>
2018-02-23 05:14:12 -0800

    Add heroku plugin. See #16.

--- rsum/heroku.tf
+++ rsum/heroku.tf
@@ -1,8 +1,9 @@
 provider "heroku" {
-  email   = "ops@company.com"
+  email   = "xander@gahancorporation.com"
   api_key = "${var.heroku_api_key}"
 }
 
-resource "heroku_app" "default" {
-  # ...
+resource "heroku_app" "dev" {
+  name   = "rsum-dev"
+  region = "us" 
 }
