eadf2f182bdedf90ba3aeea6265010711def62e3
E. A. Harris <duchess@gahan-corporation.com>
2018-03-11 03:23:14 +0000

    Add config. See #29.

---
+++ magento/bitnami/magento/htdocs/app/etc/env.php
@@ -0,0 +1,62 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'c442ea63425cef15e1eb3fabdd6eca5a',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'mariadb:3306',
+        'dbname' => 'bitnami_magento',
+        'username' => 'bn_magento',
+        'password' => 'magento_db_password',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sat, 10 Mar 2018 22:17:48 +0000',
+  ),
+);
